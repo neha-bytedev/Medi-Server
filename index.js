d0d4fb7d6f47be2e85216d23e8734dc58d98effb
neha-bytedev <neha.saman567@gmail.com>
2026-01-13 11:56:57 +0530

    Changes

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://medilink-theta-two.vercel.app"],
     credentials: true,
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: ["Content-Type", "Authorization"]
